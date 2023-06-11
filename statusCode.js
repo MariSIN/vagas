8a14bbf0a78a28630401db2527be4299320c2e4e
Mariana Nascimento <mariana.sn.silva@gmail.com>
2023-06-11 15:01:24 -0300

    feat(teste2): POST users com middleware

--- statusCode.js
+++ statusCode.js
@@ -2,6 +2,7 @@ const statusCode = {
     ok: 200,
     notFound: 404,
     created: 201,
+    conflict: 409,
     badRequest: 400,
     unauthorized: 401,
     noContent: 204,
--- teste2.js
+++ teste2.js
@@ -1,17 +1,28 @@
-var data =  require("./fakeData");
+const data = require('./fakeData');
+const { statusCode } = require('./statusCode');
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
-    var newUser = {
-        name: name,
-        job: job,
-    }
+function postUser(req, res) {
+	const { name, job } = req.body;
 
-    data.push(newUser)
-    
-    res.send(newUser);
+	const lastId = data.length > 0 ? data[data.length - 1].id : 0;
+	const newId = lastId + 1;
 
-};
+	if (!name || !job) {
+		return res
+			.status(statusCode.badRequest)
+			.json({ message: 'Todos os campos são requeridos' });
+	}
+
+	const existingUser = data.find((user) => user.name === name);
+	if (existingUser) {
+		return res
+			.status(statusCode.conflict)
+			.json({ message: 'Já existe um usuário com esse nome' });
+	}
+
+	data.push({ id: newId, name, job });
+	console.log(data);
+	return res.status(statusCode.created).json({ message: 'Usuário criado' });
+}
+
+module.exports = { postUser };
